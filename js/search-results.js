772c632fa333c168c16babc8bd022e3856af2176
soumithk <soumith.kumar@iiitb.org>
2020-05-02 12:16:55 +0530

    minor DEBUG : jquery search results

--- js/search-results.js
+++ js/search-results.js
@@ -3,13 +3,16 @@ $(document).ready(function(){
               console.log($('.search_field').val());
               $.ajax({
                   url: "GetSearchResults",
-                  type: "GET",
+                  type: "POST",
                   cache: false,
                   data: { 'search_text' : $('.search_field').val() },
                   success: function(data){
                     $('#search-results').append(data);
                   }
               });
-              $( "#ebooks-search-results" ).load( "./search_element.jsp");
+              // $( "#ebooks-search-results" ).load( "./search_element.jsp");
+              $.get("./search_element.jsp", function (data) {
+                    $("#search-results").html(data);
+                });
           });
         });
