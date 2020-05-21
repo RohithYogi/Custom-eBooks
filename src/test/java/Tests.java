53dcf71b312313c9832606398e7549cc3ddd99ff
Arunaav <arunavreddy007@gmail.com>
2020-05-21 18:20:09 +0530

    pushing test files

---
+++ src/test/java/Tests.java
@@ -0,0 +1,31 @@
+import org.junit.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class Tests {
+
+	private WebDriver driver;
+
+	@Before
+	public void setUp() {
+		// Create a new instance of the html unit driver
+		driver = new HtmlUnitDriver();
+
+		//Navigate to desired web page
+		driver.get("http://localhost:8080/Custom-eBooks");
+	}
+
+	@Test
+	public void WebsiteUpTest() {
+
+    String expectedTitle = "Custom eBooks";
+
+    String actualTitle = driver.getTitle();
+    Assert.assertEquals(expectedTitle, actualTitle);
+
+  }
+
+}
